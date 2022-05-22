464c7ecd867408683e4516f063821681350a8040
HoneyBounes <mihailostojanovic.ms@gmail.com>
2022-05-22 22:34:13 +0200

    initialcommit

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ document.getElementById("overlay").style.display = "block";
 var deonica1slika = document.getElementById("slika1");
 var deonica2slika = document.getElementById("slika2");
 var deonica3slika = document.getElementById("slika3");
+var deonica4slika = document.getElementById("slika4");
 
 //slike prodjae
 var pdeonica1slika = document.getElementById("slika11");
 var pdeonica2slika = document.getElementById("slika12");
 var pdeonica3slika = document.getElementById("slika13");
+var pdeonica4slika = document.getElementById("slika14");
 
 //varijable
 var bool = false;
@@ -21,15 +23,18 @@ var novcanik = 5;
 var deonica1 = 0;
 var deonica2 = 0;
 var deonica3 = 0;
+var deonica4 = 0;
 var cdeonica1 = 1;
 var cdeonica2 = 2;
 var cdeonica3 = 3;
+var cdeonica4 = 4;
 
 //interval popunjavanja
 var timeout = setTimeout(function(){
     document.getElementById("cdeonica1").innerHTML = cdeonica1 + "&#128176;";
     document.getElementById("cdeonica2").innerHTML = cdeonica2 + "&#128176;";
     document.getElementById("cdeonica3").innerHTML = cdeonica3 + "&#128176;";
+    document.getElementById("cdeonica4").innerHTML = cdeonica4 + "&#128176;";
     document.getElementById("novcanik").innerHTML = novcanik + "&#128176;";
 },1);
 
@@ -85,6 +90,23 @@ var interval = setInterval(function(){
 }
 },5000);
 
+var interval = setInterval(function(){
+    if(bool==true){
+    var random = Math.random();
+    random *= Math.round(Math.random()) ? 1 : -1;
+    cdeonica4 += random;
+    if(cdeonica4<-1)
+    {
+        cdeonica4 += 4;
+    }
+    if(cdeonica4>13)
+    {
+        cdeonica4 -= 6;
+    }
+    document.getElementById("cdeonica4").innerHTML = cdeonica4.toFixed(2) + "&#128176;";
+}
+},5000);
+
 //kupovina deonica
 deonica1slika.onclick = function(){
     if(novcanik>=cdeonica1 && cdeonica1>=0){
@@ -113,6 +135,15 @@ deonica3slika.onclick = function(){
     }
 }
 
+deonica4slika.onclick = function(){
+    if(novcanik>=cdeonica4 && cdeonica4>=0){
+    deonica4 += 1;
+    document.getElementById("brdeonica1").innerHTML = deonica1;
+    novcanik -= cdeonica4;
+    document.getElementById("novcanik").innerHTML = novcanik.toFixed(2) + "&#128176;";
+    }
+}
+
 //prodaja deonica
 pdeonica1slika.onclick = function(){
     if(deonica1>0 && cdeonica1>=0){
@@ -137,6 +168,15 @@ pdeonica3slika.onclick = function(){
     deonica3 -= 1;
     document.getElementById("brdeonica3").innerHTML = deonica3;
     novcanik += cdeonica3;
+    document.getElementById("novcanik").innerHTML = novcanik.toFixed(2) + "&#128176;";
+    }
+}
+
+pdeonica4slika.onclick = function(){
+    if(deonica4>0 && cdeonica4>=0){
+    deonica4 -= 1;
+    document.getElementById("brdeonica1").innerHTML = deonica4;
+    novcanik += cdeonica4;
     document.getElementById("novcanik").innerHTML = novcanik.toFixed(2) + "&#128176;";
     }
 }
